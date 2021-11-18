6c86a3f2d2799c833cd9c25de12462fc818b2831
Admin <Admin@LAPTOP-798FHRV5>
2021-11-18 21:37:38 +1300

    Add project files.

---
+++ Migrations/20211117100229_last.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hire_Professionals_Final_Project.Migrations
+{
+    public partial class last : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/Hire_ProfessionalsIdentity/20211114085428_first.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hire_Professionals_Final_Project.Migrations.Hire_ProfessionalsIdentity
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
